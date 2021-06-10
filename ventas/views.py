70f3ca816ae2b21e07e776d463fe0d8dbe98f103
Claure <cclaurev@est.emi.edu.bo>
2021-06-10 17:44:07 -0400

    Creacion de cuenta, pagina personal

--- ventas/views.py
+++ ventas/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render, HttpResponse, redirect
 from .models.producto import Producto
 from .models.proveedor import Proveedor
 from .models.usuario import Usuario
+from .models.carrito import Carrito
 from django.db.models import ObjectDoesNotExist
 from ast import literal_eval
+from django.contrib import messages
 
 carrito_compra = []
 cantidades = []
 sesion = None
 # Create your views here.
+# CATALOGO
 def home(request):
     productos = Producto.objects.all()
+    # messages.add_message(request, messages.SUCCESS, 'Mensaje recibido')
     return render(request, 'ventas/home.html', {'productos':productos, 'carrito':carrito_compra, 'sesion':sesion})
 
 def busqueda(request):
@@ -19,19 +23,20 @@ def busqueda(request):
     print(busqueda)
     productos = Producto.objects.filter(nombre__icontains=busqueda)
     print(productos)
-    return render(request, 'ventas/home.html', {'productos':productos, 'carrito':carrito_compra})
+    return render(request, 'ventas/home.html', {'productos':productos, 'carrito':carrito_compra, 'sesion':sesion})
 
 def producto(request, id):
     producto = Producto.objects.get(id=id)
     producto.caracteristicas = literal_eval(producto.caracteristicas)
-    return render(request, 'ventas/producto.html', {'producto':producto,'carrito':carrito_compra})
+    return render(request, 'ventas/producto.html', {'producto':producto,'carrito':carrito_compra, 'sesion':sesion})
 
 def categoria(request, categoria):
     print(categoria)
     productos = Producto.objects.filter(categoria=categoria)
     print(productos)
-    return render(request, 'ventas/home.html', {'productos':productos})
+    return render(request, 'ventas/home.html', {'productos':productos, 'sesion':sesion})
 
+# CARRITO
 def carrito(request):
     costo_total = 0
     cant_productos = []
@@ -42,11 +47,13 @@ def carrito(request):
         costo_total += precio
         cant_productos.append((cant, prod, precio))
     print(cant_productos)
-    data = {'cant_productos':cant_productos, 'total':costo_total}
+    data = {'cant_productos':cant_productos, 'total':costo_total, 'sesion':sesion}
     return render(request, 'ventas/carrito.html', data)
 
 def add_carrito(request, id):
     carrito_compra.append(int(id))
+    messages.success(request, 'Producto agregado al carrito')
+    print('request ->  ', request.path)
     try:
         form_data = request.POST.dict()
         cantidades.append(int(form_data['cantidad']))
@@ -55,6 +62,7 @@ def add_carrito(request, id):
         return redirect('home')
     print(carrito_compra)
     print(cantidades)
+    
     return redirect(f'/producto/{id}')
 
 def remover_carrito(request, id):
@@ -65,15 +73,52 @@ def remover_carrito(request, id):
 
 def confirmar_pedido(request):
     if sesion:
+        carrito = Carrito.objects.create(
+            
+        )
         for cantidad, producto in zip(cantidades, carrito_compra):
-            print(Producto.objects.get(id=producto))
+            producto = Producto.objects.get(id=producto)
+            producto.stock -= cantidad
+            producto.save()
+            carrito = Carrito.objects.create(
+
+            )
         carrito_compra.clear()
         cantidades.clear()
+        messages.success(request, 'Pedido confirmado exitosamente')
         return redirect('home')
     else:
+        messages.warning(request, 'Debe iniciar sesion')
         return redirect('carrito')
 
+# USUARIO
 def nueva_cuenta(request):
+    if request.method == 'POST':
+        form_data = request.POST.dict()
+        contra = form_data['contrasenia']
+        confirm = form_data['contrasenia_confirm']
+        if contra == confirm:
+            print('CONFIRMADO')
+            try:
+                usuario = Usuario.objects.create(
+                    nombre_completo=form_data['nombre'],
+                    fecha_de_nacimiento=form_data['fecha_nacimiento'],
+                    celular=form_data['telefono'],
+                    direccion=form_data['direccion'],
+                    correo=form_data['correo'],
+                    contrasenia=contra
+                )
+            except:
+                messages.warning(request, 'Telefono o correo ya existentes')
+                return redirect('nueva-cuenta')
+            global sesion
+            sesion = usuario.correo
+            print(sesion)
+            messages.success(request, 'Cuenta creada exitosamente')
+            return redirect('mi-cuenta')
+        else:
+            messages.warning(request, 'Las contraseñas no coninciden')
+            return redirect('nueva-cuenta')
     return render(request, 'ventas/registro_usuario.html')
 
 def iniciar_sesion(request):
@@ -86,16 +131,23 @@ def iniciar_sesion(request):
         global sesion
         sesion = usuario.correo
         print(usuario, sesion)
-
+        messages.success(request, 'Sesion iniciada')
     except ObjectDoesNotExist:
-        print('SESION NO INICIADA')
+        messages.warning(request, 'Usuario no existente')
 
     return redirect('home')
 
 def cerrar_sesion(request):
     global sesion
     sesion = None
+    messages.warning(request, 'Sesion cerrada')
     return redirect('home')
 
 def mi_cuenta(request):
-    return render(request, 'ventas/cuenta_usuario.html')
+    usuario = Usuario.objects.get(correo=sesion)
+    return render(request, 'ventas/cuenta_usuario.html', {'sesion':sesion, 'usuario':usuario})
+
+def historial_carrito(request):
+    print('consulta a base de datos de las compras')
+    print('envio de toda la informacion')
+    return render(request, 'ventas/historial_carrito.html')
